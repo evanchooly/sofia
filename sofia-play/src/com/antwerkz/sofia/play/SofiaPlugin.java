b29681e5600740f6793833165cd0faa1bb4350f4
evanchooly <jlee@antwerkz.com>
2012-06-29 00:03:09 -0400

    more maven goodness

--- sofia-play/src/com/antwerkz/sofia/play/SofiaPlugin.java
+++ sofia-play/src/com/antwerkz/sofia/play/SofiaPlugin.java
@@ -8,28 +8,29 @@ import com.antwerkz.sofia.SofiaConfig;
 import play.PlayPlugin;
 
 public class SofiaPlugin extends PlayPlugin {
-    private File properties = new File("conf/messages");
-    private File targetDir = new File("app");
+  private File properties = new File("conf/messages");
+  private File targetDir = new File("app");
 
   @Override
-    public void onApplicationStart() {
-        generate();
-    }
+  public void onApplicationStart() {
+    generate();
+  }
 
-    @Override
-    public void detectChange() {
-        generate();
-    }
+  @Override
+  public void detectChange() {
+    generate();
+  }
 
-    private void generate() {
-      try {
-        new LocalizerGenerator(new SofiaConfig()
-          .setPackageName("utils")
-          .setProperties(properties)
-          .setOutputDirectory(targetDir)
-          .setUseControl(true)).write();
-      } catch (IOException e) {
-        throw new RuntimeException(e.getMessage(), e);
-      }
+  private void generate() {
+    try {
+      new LocalizerGenerator(new SofiaConfig()
+        .setPackageName("utils")
+        .setProperties(properties)
+        .setOutputDirectory(targetDir)
+        .setUseControl(true))
+        .write();
+    } catch (IOException e) {
+      throw new RuntimeException(e.getMessage(), e);
     }
+  }
 }
