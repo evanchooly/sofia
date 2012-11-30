1199822d549d126f12e8f256680dc06408b45bc0
evanchooly <jlee@antwerkz.com>
2012-11-29 23:22:38 -0500

    generated demo js file

---
+++ tests/src/main/webapp/js/sofia.js
@@ -0,0 +1,23 @@
+sofia = {
+        another: function() {
+        return "I'm an error";
+    },
+    dateProperty: function(arg0, arg1) {
+        return "Today''s date {0,date,full} and now a number {1,number}";
+    },
+    me: function() {
+        return "I'm just a warning, though.";
+    },
+    lonely: function() {
+        return "I'm only in the default bundle.";
+    },
+    newProperty: function() {
+        return "New Property";
+    },
+    parameterizedPropertyLongName: function(arg0, arg1) {
+        return "I need parameters {0} and {1}";
+    },
+    testProperty: function() {
+        return "I'm the first test property";
+    }
+};
